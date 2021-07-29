cee1f6dec9af1acedd7380b8b44ee84d3eb54d18
Sanghun OH <oshosh12@gmail.com>
2021-07-30 00:56:06 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -20,6 +20,7 @@ soup = BeautifulSoup(data.text, 'html.parser')
 trs = soup.select('#old_content > table > tbody > tr')
 
 for tr in trs:
+    
     a_tag = tr.select_one('td.title > div > a')
     if a_tag is not None:
         rank = tr.select_one('td:nth-child(1) > img')['alt']
