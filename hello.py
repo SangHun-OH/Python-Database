e8dce0853389614808dffcdf8b4cbcf27c8a4cee
Sanghun OH <oshosh12@gmail.com>
2021-08-02 00:43:50 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -16,7 +16,7 @@ data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&d
 # soup이라는 변수에 "파싱 용이해진 html"이 담긴 상태가 됨
 # 이제 코딩을 통해 필요한 부분을 추출하면 된다.
 soup = BeautifulSoup(data.text, 'html.parser')
-
+ 
 
 trs = soup.select('#old_content > table > tbody > tr')
 
