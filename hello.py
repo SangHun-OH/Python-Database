560c6133800a162628c8557260cc5740a543b782
Sanghun OH <oshosh12@gmail.com>
2021-09-06 19:59:29 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -9,6 +9,7 @@ db = client.dbsparta
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 
 
+
 #Ajax와 같은 기능
 data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303',headers=headers)
 
